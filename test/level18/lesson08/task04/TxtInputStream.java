c408c896d0f5ac33f5c91864abcf54ad3b063ba1
deft1991 <deft1991@mail.ru>
2016-02-17 14:52:16 +0300

    com.javarush.test.level18.lesson08.task04

---
+++ test/level18/lesson08/task04/TxtInputStream.java
@@ -0,0 +1,36 @@
+package com.javarush.test.level18.lesson08.task04;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/* UnsupportedFileName
+Измените класс TxtInputStream так, чтобы он работал только с txt-файлами (*.txt)
+Например, first.txt или name.1.part3.txt
+Если передан не txt-файл, например, file.txt.exe, то конструктор должен выбрасывать исключение UnsupportedFileNameException
+*/
+
+public class TxtInputStream extends FileInputStream {
+    public TxtInputStream(String fileName) throws FileNotFoundException, UnsupportedFileNameException
+    {
+        super(fileName);
+        //getFIleExtention(fileName);
+       if(!(fileName.endsWith(".txt")))
+           throw new UnsupportedFileNameException();
+    }
+  /*  public String getFIleExtention(String fileName) throws UnsupportedFileNameException
+    {
+        int dotPos = fileName.lastIndexOf(".");
+        if (fileName.substring(dotPos, (fileName.length())).equals(".txt"))
+        {
+            return fileName;
+        }
+        else throw new UnsupportedFileNameException();
+    }*/
+
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedFileNameException
+    {
+        TxtInputStream txt = new TxtInputStream("C:\\temp\\file1.dxt");
+    }
+}
+
