fa68be38ca5805bb9a15d87d1534617ccdc5435a
deft1991 <deft1991@gmail.com>
2016-04-15 16:52:16 +0300

    com.javarush.test.level18.lesson10.home09

--- test/level19/lesson10/bonus01/Solution.java
+++ test/level19/lesson10/bonus01/Solution.java
@@ -1,9 +1,11 @@
 package com.javarush.test.level19.lesson10.bonus01;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 /* Отслеживаем изменения
 Считать в консоли 2 имени файла - file1, file2.
@@ -32,51 +34,61 @@ C:\temp\file2.txt
 
 */
 
-public class Solution {
+public class Solution
+{
     public static List<LineItem> lines = new ArrayList<LineItem>();
 
     public static void main(String[] args) throws IOException
     {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String file1Name = reader.readLine();
-        String file2Name = reader.readLine();
+        String fileName1 = reader.readLine();
+        String fileName2 = reader.readLine();
+        BufferedReader fileReader1 = new BufferedReader(new FileReader(fileName1));
+        BufferedReader fileReader2 = new BufferedReader(new FileReader(fileName2));
         reader.close();
-        File file1 = new File(file1Name);
-        File file2 = new File(file2Name);
-        FileReader scanner1 = new FileReader(file1);
-        FileReader scanner2 = new FileReader(file2);
-
-        while(scanner1.ready()||scanner2.ready()){
-
-            char s1 = (char) scanner1.read();
-            char s2 = (char) scanner2.read();
-            if (s1==(s2))
-                lines.add(new LineItem(Type.SAME,String.valueOf(s1)));
-
-            if(s1>s2)
-                lines.add(new LineItem(Type.REMOVED,String.valueOf(s1)));
-
-            if(s1<s2)
-                lines.add(new LineItem(Type.ADDED,String.valueOf(s2)));
+        String s1, s2;
+        while ((s1 = fileReader1.readLine()) != null | (s2 = fileReader2.readLine()) != null)
+        {
+            try
+            {
+               // System.out.println(s2);
+                if (s1.equals(s2))
+                    lines.add(new LineItem(Type.SAME, s1));
+                if (s1.length() > s2.length())
+                    lines.add(new LineItem(Type.REMOVED, s1));
+                if (s1.length() < s2.length())
+                    lines.add(new LineItem(Type.ADDED, s2));
+            }
+            catch (NullPointerException e){
+               if (s1==null)
+                   lines.add(new LineItem(Type.ADDED, s2));
+                if (s2==null)
+                    lines.add(new LineItem(Type.REMOVED, s1));
+            }
         }
-        scanner1.close();
-        scanner2.close();
-        for(int i =0;i<lines.size();i++)
-            System.out.println(lines.get(i).type+" "+lines.get(i).line);
+        fileReader1.close();
+        fileReader2.close();
+        /*for (int i = 0; i < lines.size(); i++)
+        {
+            System.out.println(lines.get(i).type + " " + lines.get(i).line);
+        }*/
     }
 
 
-    public static enum Type {
+    public static enum Type
+    {
         ADDED,        //добавлена новая строка
         REMOVED,      //удалена строка
         SAME          //без изменений
     }
 
-    public static class LineItem {
+    public static class LineItem
+    {
         public Type type;
         public String line;
 
-        public LineItem(Type type, String line) {
+        public LineItem(Type type, String line)
+        {
             this.type = type;
             this.line = line;
         }
