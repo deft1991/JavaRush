f21f54becfb7485a9c3dbbf4000dd4cf118ab1f6
deft1991 <deft1991@gmail.com>
2016-05-10 14:28:32 +0300

    com.javarush.test.level20.lesson02.task03

---
+++ src/com/javarush/test/level22/lesson05/task02/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.test.level22.lesson05.task02;
+
+/* Между табуляциями
+Метод getPartOfString должен возвращать подстроку между первой и второй табуляцией.
+На некорректные данные бросить исключение TooShortStringException.
+Класс TooShortStringException не менять.
+*/
+public class Solution {
+    public static String getPartOfString(String string) throws TooShortStringException {
+        if (string==null||string.isEmpty())
+            throw new TooShortStringException();
+        try {
+            int index = string.indexOf("\t");
+            String s = string.substring(index + 1);
+            string = s.substring(0, s.indexOf("\t"));
+        }
+        catch (Exception e){
+            throw new TooShortStringException();
+        }
+        return string;
+    }
+
+    public static class TooShortStringException extends Exception {
+    }
+
+    public static void main(String[] args) throws TooShortStringException {
+        System.out.println(getPartOfString("tab0\ttab\ttab1\t"));       //tab
+        System.out.println(getPartOfString("\t\t"));                    //
+        System.out.println(getPartOfString("123\t123"));                //Exception
+        System.out.println(getPartOfString(null));                      //Exception
+    }
+}
