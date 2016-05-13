cec876efa29dbec9701cb4b2a4cadbecab1b3858
deft1991 <deft1991@gmail.com>
2016-05-13 15:43:11 +0300

    com.javarush.test.level20.lesson02.task03

---
+++ src/com/javarush/test/level22/lesson18/big01/Field.java
@@ -0,0 +1,69 @@
+package com.javarush.test.level22.lesson18.big01;
+
+import java.io.File;
+
+/**
+ * Created by Golitsyn.SN on 13.05.2016.
+ */
+public class Field {
+    int width;
+    int height;
+    int matrix[][];
+
+    Field(int width,int height){
+        this.width = width;
+        this.height=height;
+        matrix = new int[height][width];
+    }
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+/**
+ * *Задание 13
+ Теперь приступим к реализации созданных методов.
+ Напиши реализацию метода print в классе Field
+ а) Метод должен выводить на экран прямоугольник.
+ б) Высота прямоугольника равна height, ширина - width
+ в) Если данная клетка пустая - вывести точку, если не пустая - английский X
+
+ Подсказка:
+ if (matrix[y][x]==0) ...
+ */
+    public void print(){
+    //  int  pole[][] = new int[height][width];
+        for (int y=0;y<getHeight();y++){
+            for (int x=0;x<getWidth();x++) {
+              if (getValue(x,y)==0)
+                System.out.print(".");
+                else
+                  System.out.print("X");
+            }
+            System.out.println();
+        }
+      //  System.out.println(pole);
+    }
+    public void removeFullLines(){}
+
+    public Integer getValue(int x, int y){
+        if (x >= 0 && x < width && y >= 0 && y < height)
+            return matrix[y][x];
+        return null;
+    }
+    public void setValue(int x, int y, int value){
+        matrix[y][x]=value;
+    }
+
+    public static void main(String[] args) {
+        Field fff = new Field(10,20);
+        fff.print();
+    }
+
+}
