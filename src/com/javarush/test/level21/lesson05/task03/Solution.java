fb8a0c7a94cd13fb3a4da09dd53d3ccb38f3a44d
deft1991 <deft1991@gmail.com>
2016-05-18 10:27:52 +0300

    com.javarush.test.level20.lesson02.task03

--- src/com/javarush/test/level21/lesson05/task03/Solution.java
+++ src/com/javarush/test/level21/lesson05/task03/Solution.java
@@ -1,16 +1,23 @@
 package com.javarush.test.level21.lesson05.task03;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /* Ошибка в equals/hashCode
 Исправьте ошибки реализаций методов equals и hashCode для класса Solution
 */
+
 public class Solution {
     private int anInt;
     private String string;
     private double aDouble;
     private Date date;
     private Solution solution;
+
+    public static void main(String[] args) {
+        Date d = null;
+        System.out.println(d.hashCode());
+    }
 
     public Solution(int anInt, String string, double aDouble, Date date, Solution solution) {
         this.anInt = anInt;
@@ -23,15 +30,15 @@ public class Solution {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o instanceof Solution) return false;
+        if (!(o instanceof Solution)) return false;
 
         Solution solution1 = (Solution) o;
 
         if (Double.compare(solution1.aDouble, aDouble) != 0) return false;
         if (anInt != solution1.anInt) return false;
-        if (date != null ? !date.equals(solution1.date) : solution1.date == null) return false;
-        if (solution != null ? !solution.equals(solution1.solution) : solution1.solution == null) return false;
-        if (string != null ? !string.equals(solution1.string) : solution1.string == null) return false;
+        if (date != null ? !date.equals(solution1.date) : solution1.date != null) return false;
+        if (solution != null ? !solution.equals(solution1.solution) : solution1.solution != null) return false;
+        if (string != null ? !string.equals(solution1.string) : solution1.string != null) return false;
 
         return true;
     }
@@ -41,8 +48,10 @@ public class Solution {
         int result;
         long temp;
         result = anInt;
+        result = 31 * result + (string !=null ? string.hashCode():0);
         temp = aDouble != +0.0d ? Double.doubleToLongBits(aDouble) : 0L;
         result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (date !=null ? date.hashCode(): date.hashCode());
         result = 31 * result + (solution != null ? solution.hashCode() : 0);
         return result;
     }
