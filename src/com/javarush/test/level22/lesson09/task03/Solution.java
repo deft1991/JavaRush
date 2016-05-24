0756896128b65945ac46fa16980934b6b6a3deb2
deft1991 <deft1991@gmail.com>
2016-05-24 08:13:11 +0300

    com.javarush.test.level20.lesson02.task03

--- src/com/javarush/test/level22/lesson09/task03/Solution.java
+++ src/com/javarush/test/level22/lesson09/task03/Solution.java
@@ -1,12 +1,7 @@
 package com.javarush.test.level22.lesson09.task03;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 /* Составить цепочку слов
-+ В методе main считайте с консоли имя файла, который содержит слова, разделенные пробелом.
+В методе main считайте с консоли имя файла, который содержит слова, разделенные пробелом.
 В методе getLine используя StringBuilder расставить все слова в таком порядке,
 чтобы последняя буква данного слова совпадала с первой буквой следующего не учитывая регистр.
 Каждое слово должно участвовать 1 раз.
@@ -21,20 +16,13 @@ import java.io.InputStreamReader;
 Амстердам Мельбурн Нью-Йорк Киев Вена
 */
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         //...
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedReader fileReader = new BufferedReader(new FileReader(reader.readLine()));
         StringBuilder result = getLine();
         System.out.println(result.toString());
-
-        reader.close();
-        fileReader.close();
     }
 
     public static StringBuilder getLine(String... words) {
-        StringBuilder sb = new StringBuilder();
-
           return null;
     }
 }
