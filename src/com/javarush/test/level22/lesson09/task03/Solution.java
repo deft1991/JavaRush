6ea64ef2afdace80903b826a68592d77cd290810
deft <deft1991@gmail.com>
2016-07-19 16:29:53 +0300

    Копировал с гид хаба, считаю что мое решение было вернее и понятнее

--- src/com/javarush/test/level22/lesson09/task03/Solution.java
+++ src/com/javarush/test/level22/lesson09/task03/Solution.java
@@ -7,26 +7,23 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-	/* Составить цепочку слов
-	В методе main считайте с консоли имя файла, который содержит слова, разделенные пробелом.
-	В методе getLine используя StringBuilder расставить все слова в таком порядке,
-	чтобы последняя буква данного слова совпадала с первой буквой следующего не учитывая регистр.
-	Каждое слово должно участвовать 1 раз.
-	Метод getLine должен возвращать любой вариант.
-	Слова разделять пробелом.
+/* Составить цепочку слов
+В методе main считайте с консоли имя файла, который содержит слова, разделенные пробелом.
+В методе getLine используя StringBuilder расставить все слова в таком порядке,
+чтобы последняя буква данного слова совпадала с первой буквой следующего не учитывая регистр.
+Каждое слово должно участвовать 1 раз.
+Метод getLine должен возвращать любой вариант.
+Слова разделять пробелом.
+В файле не обязательно будет много слов.
 
-	Пример тела входного файла:
-	Киев Нью-Йорк Амстердам Вена Мельбурн
+Пример тела входного файла:
+Киев Нью-Йорк Амстердам Вена Мельбурн
 
-	Результат:
-	Амстердам Мельбурн Нью-Йорк Киев Вена
-
-	*/
-
+Результат:
+Амстердам Мельбурн Нью-Йорк Киев Вена
+*/
 public class Solution {
-    public static void main(String[] args) throws IOException
-    {
-        //...
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         String fileName = scanner.nextLine();
         ArrayList<String> list = new ArrayList<>();
@@ -42,38 +39,75 @@ public class Solution {
         StringBuilder result = getLine(words);
         System.out.println(result.toString());
         scanner.close();
+
     }
 
-    public static StringBuilder getLine(String... words)
-    {
-        ArrayList<String> strings = new ArrayList<>();
-        Collections.addAll(strings, words);
-        StringBuilder sb = new StringBuilder();
-        if (strings.size() == 0)
-            return new StringBuilder();
+    public static StringBuilder getLine(String... words) {
+        if (words == null) {return new StringBuilder();}
+        if (words.length==0) {return new StringBuilder();}
 
-        sb.append(strings.get(0));
-        strings.remove(0);
+        //String[] array = i.split(" ");
+        ArrayList<String> arrayList = new ArrayList<>();
+        Collections.addAll(arrayList,words);
+        int neededCounter = arrayList.size();
+        StringBuilder resultBuilder = new StringBuilder();
+        while (true)
+        {
+            int wordCounter = 0;
+            ArrayList<String> temp = new ArrayList<>(arrayList);
+            Collections.shuffle(temp);
+            StringBuilder tempBuilder = new StringBuilder();
+            tempBuilder.append(arrayList.get(0));
+            temp.remove(arrayList.get(0));
+            boolean canAdd = true;
 
-        while (strings.size()>0){
-            for (int i = 0; i < strings.size(); i++) {
-                String a = strings.get(i).toUpperCase().toLowerCase();
-                String b = sb.toString().toUpperCase().toLowerCase();
-                if (a.charAt(0) == b.charAt(sb.length() - 1))
-                { // в конец
-                    sb.append(" ").append(strings.get(i));
-                    strings.remove(i);
-                    continue;
+            while (canAdd)
+            {
+                {
+                    ArrayList<String> toDelete = new ArrayList<>();
+                    for (String s : temp)
+                    {
+                        StringBuilder word = new StringBuilder(s);
+                        // если конец стрингбилдера равен первой букве другого слова
+                        if (tempBuilder.substring(tempBuilder.length() - 1).equals(word.reverse().substring(s.length() - 1).toLowerCase()))
+                        {
+                            tempBuilder.append(" " + s);
+                            toDelete.add(s);
+                            wordCounter++;
+                            continue;
+                        }
+                        // если начало стрингбилдера равно последней букве другого слова "Киев Вена" - "Нью Йорк"
+                        if (tempBuilder.toString().substring(0, 1).toLowerCase().equals(s.substring(s.length() - 1).toLowerCase()))
+                        {
+                            tempBuilder.reverse().append(" " + word);
+                            tempBuilder.reverse();
+                            toDelete.add(s);
+                            wordCounter++;
+                            continue;
+                        }
+                    }
+                    // удаляем уже вставленные слова
+                    for (String s : toDelete)
+                    {
+                        temp.remove(s);
+                    }
+                    toDelete.clear();
+                    // если ни к концу ни к началу нельзя добавить символ - break;
+                    for (String s : temp)
+                    {
+                        StringBuilder word = new StringBuilder(s);
+                        if (!tempBuilder.substring(tempBuilder.length() - 1).equals(word.reverse().substring(s.length() - 1).toLowerCase()) &&
+                                !tempBuilder.toString().substring(0, 1).toLowerCase().equals(s.substring(s.length() - 1).toLowerCase()))
+                        {
+                            canAdd = false;
+                        }
+                    }
                 }
-
-                if (b.charAt(0) == a.charAt(a.length() - 1))
-                { //в начало
-                    sb.insert(0, " ");
-                    sb.insert(0, strings.get(i));
-                    strings.remove(i);
+                resultBuilder = tempBuilder;
+                if (wordCounter==neededCounter-1) {
+                    return resultBuilder;
                 }
             }
         }
-        return sb;
     }
 }
