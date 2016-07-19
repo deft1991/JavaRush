1f63dea1399038cddf74b235d773c5a0fec29969
deft <deft1991@gmail.com>
2016-07-19 11:18:14 +0300

    in progress

--- src/com/javarush/test/level22/lesson09/task03/Solution.java
+++ src/com/javarush/test/level22/lesson09/task03/Solution.java
@@ -1,5 +1,12 @@
 package com.javarush.test.level22.lesson09.task03;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
 /* Составить цепочку слов
 В методе main считайте с консоли имя файла, который содержит слова, разделенные пробелом.
 В методе getLine используя StringBuilder расставить все слова в таком порядке,
@@ -14,15 +21,41 @@ package com.javarush.test.level22.lesson09.task03;
 
 Результат:
 Амстердам Мельбурн Нью-Йорк Киев Вена
+
+/home/user/temp/file1.txt
 */
 public class Solution {
     public static void main(String[] args) {
         //...
-        StringBuilder result = getLine();
-        System.out.println(result.toString());
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String fileName = reader.readLine();
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            String stroka = br.readLine();
+            // закомитить перед тестом
+            System.out.println(stroka);
+            String [] slova = stroka.trim().split(" ");
+            StringBuilder result = getLine(slova);
+            System.out.println(result.toString());
+            System.out.println((int)(Math.random()*slova.length));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     public static StringBuilder getLine(String... words) {
-          return null;
+        int rand = (int) (Math.random()*words.length);
+        ArrayList<String>list = new ArrayList<>();
+        // добавил все слова в лист
+        Collections.addAll(list, words);
+        StringBuilder sb = new StringBuilder();
+        sb.append(list.get(rand));
+        list.remove(rand);
+        for (int i = 0; i< list.size();i++){
+
+        }
+          return sb;
     }
 }
